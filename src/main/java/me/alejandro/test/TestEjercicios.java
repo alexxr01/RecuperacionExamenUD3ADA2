d8f3b79559a05e11ade53be8b922a33a80ea3661
Alex <aruibon154@g.educaand.es>
2023-03-19 23:50:08 +0100

    Avances

--- src/main/java/me/alejandro/test/TestEjercicios.java
+++ src/main/java/me/alejandro/test/TestEjercicios.java
@@ -1,7 +1,9 @@
 package me.alejandro.test;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
@@ -112,6 +114,11 @@ public class TestEjercicios {
 	}
 
 	public static void apartadoC() {
+		em.getTransaction().begin();
+		
+		/*
+		 * 1
+		 */
 		Alumno alumno7 = em.find(Alumno.class, 7);
 		List<Asignatura> asignaturasMatriculadas = alumno7.getAsignaturas();
 
@@ -119,7 +126,158 @@ public class TestEjercicios {
 		for (Asignatura asignatura : asignaturasMatriculadas) {
 		    System.out.println(asignatura.getDASIG());
 		}
+		
+		/*
+		 * 2
+		 */
+		Alumno resultado = (Alumno) em.createQuery("SELECT a FROM Alumno a JOIN a.asignaturas asig WHERE asig.nombre = 'Acceso a Datos'")
+				.getSingleResult();
+		System.out.println(resultado);
+		
+		/*
+		 * 3
+		 */
+		List<Profesor> profesores = em.createQuery("SELECT p FROM Profesor p", Profesor.class).getResultList();
 
+	    for (Profesor profesor : profesores) {
+	        System.out.println("El profesor " + profesor.getNOMP() + " imparte las siguientes asignaturas:");
+	        for (Asignatura asignatura : profesor.getAsignatura()) {
+	            System.out.println("- " + asignatura.getDASIG());
+	        }
+	    }
+	    
+	    em.close();
+
+	}
+	
+	public static void apartadoD(String nombreAlumno) {
+		/*
+		 * 1
+		 */
+		String consulta = "SELECT a.noma FROM Alumno a JOIN a.matriculas m JOIN m.asignatura asi WHERE asi.dasig = 'bases de datos'";
+
+		List<String> nombres = em.createQuery(consulta, String.class).getResultList();
+
+		for (String nombre : nombres) {
+		    System.out.println(nombre);
+		}
+
+		/*
+		 * 2
+		 */
+		// Encontrar el alumno con el nombre dado
+	    Alumno alumno = em
+	            .createQuery("SELECT a FROM Alumno a WHERE a.nombre = :nombre", Alumno.class)
+	            .setParameter("nombre", nombreAlumno)
+	            .getSingleResult();
+
+	    // Obtener la lista de asignaturas en las que está matriculado el alumno
+	    List<Asignatura> asignaturas = alumno.getAsignaturas();
+
+	    // Imprimir la lista de asignaturas
+	    System.out.println("Asignaturas en las que está matriculado " + nombreAlumno + ":");
+	    for (Asignatura asignatura : asignaturas) {
+	        System.out.println(asignatura.getDASIG() + " - " + asignatura.getCUR());
+	    }
+	    
+	    /*
+	     * 3
+	     */
+	    
+	    /*
+	     * 4
+	     */
+	    // Obtener el alumno "Fernando Riera Perera"
+	    Alumno alumnoBuscar = em.createQuery("SELECT a FROM Alumno a WHERE a.nombre = 'Fernando Riera Perera'", Alumno.class).getSingleResult();
+
+	    // Obtener las asignaturas del alumno
+	    List<Asignatura> asignaturasAlumno = alumnoBuscar.getAsignaturas();
+
+	    // Obtener los profesores que imparten cada una de las asignaturas
+	    Set<Profesor> profesores = new HashSet<>();
+	    for (Asignatura asignatura : asignaturasAlumno) {
+	        profesores.add(asignatura.getProfesor());
+	    }
+
+	    // Mostrar la lista de profesores
+	    System.out.println("Los profesores que dan clase al alumno Fernando Riera Perera son:");
+	    for (Profesor profesor : profesores) {
+	        System.out.println("- " + profesor.getNOMP());
+	    }
+
+	}
+	
+	public static void apartadoE() {
+		/*
+		 * 1
+		 */
+		em.getTransaction().begin();
+		Alumno alumno5 = em.find(Alumno.class, 5); // Buscamos el alumno 5 en la BD
+		Asignatura asignatura12 = em.find(Asignatura.class, 12); // Buscamos la asignatura 12 en la BD
+
+		alumno5.setAsignaturas((List<Asignatura>) asignatura12); // Agregamos la asignatura 12 al alumno 5
+
+		em.persist(alumno5); // Actualizamos los cambios en la BD
+		em.getTransaction().commit();
+		
+		/*
+		 * 2
+		 */
+		em.getTransaction().begin();
+		Profesor profesorCambiarNombre = em.find(Profesor.class, 4L);
+		profesorCambiarNombre.setNOMP("NUEVO NOMBRE PROFESOR FECHERITO");
+		em.getTransaction().commit();
+		
+		/*
+		 * 3
+		 */
+		// Crear la nueva asignatura
+		Asignatura nuevaAsignatura = new Asignatura();
+		nuevaAsignatura.setDASIG("Nueva asignatura");
+		nuevaAsignatura.setCUR(String.valueOf(2));
+
+		// Crear el nuevo profesor
+		Profesor nuevoProfesor = new Profesor();
+		nuevoProfesor.setNOMP("Nuevo profesor");
+		nuevoProfesor.setCPROF(23);
+
+		// Asociar la nueva asignatura con el nuevo profesor
+		nuevoProfesor.setAsignatura((List<Asignatura>) nuevaAsignatura);
+
+		// Asociar el alumno 5 con la nueva asignatura
+		Alumno cambiarAlumno5 = em.find(Alumno.class, 5);
+		cambiarAlumno5.setAsignaturas((List<Asignatura>) nuevaAsignatura);
+
+		// Guardar los cambios en la base de datos
+		em.getTransaction().begin();
+		em.persist(nuevaAsignatura);
+		em.persist(nuevoProfesor);
+		em.getTransaction().commit();
+		
+		/*
+		 * 4
+		 */
+		// Obtener el profesor 6
+		Profesor profesor6 = em.find(Profesor.class, 6);
+
+		// Crear el nuevo profesor
+		Profesor nuevoProfesor4 = new Profesor();
+		nuevoProfesor4.setNOMP("Juan Pérez");
+
+		// Obtener las asignaturas en las que imparte el profesor 6
+		List<Asignatura> asignaturas = em.createQuery("SELECT a FROM Asignatura a WHERE a.profesor = :profesor", Asignatura.class)
+		        .setParameter("profesor", profesor6)
+		        .getResultList();
+
+		// Actualizar el profesor en cada asignatura
+		for (Asignatura asignatura : asignaturas) {
+		    asignatura.setProfesor(nuevoProfesor4);
+		}
+
+		// Persistir los cambios en la base de datos
+		em.getTransaction().begin();
+		em.persist(nuevoProfesor4);
+		em.getTransaction().commit();
 	}
 
 }
